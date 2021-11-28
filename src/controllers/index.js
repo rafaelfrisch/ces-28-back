bd62a895e79015c4dbe86df290f0866e87c8710e
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-11-28 15:03:47 -0300

    Order

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,3 +2,4 @@ export * from './auth';
 export * from './users';
 export * from './products';
 export * from './category';
+export * from './orders';
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const userRouter = require('./users');
 const productRouter = require('./products');
 const categoryRouter = require('./category');
+const orderRouter = require('./orders');
 
 module.exports = function(app){
     app.use(userRouter)
     app.use(productRouter)
     app.use(categoryRouter)
+    app.use(orderRouter)
 }
