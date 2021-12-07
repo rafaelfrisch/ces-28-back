baa3f8e195900d3cfbf9d665af2cffa3abb6d446
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-12-07 00:40:26 -0300

    Create multiple random orders via command prompt

--- src/queries/createOrders.js
+++ src/queries/createOrders.js
@@ -1,8 +1,12 @@
-import * as models from '../models';
+import * as utils from '../utils';
+import chalk from 'chalk'
 
 export const createOrders = async (numOrders, minDate, maxDate) => {
-    console.log('entrou')
     try {
+        
+        for(let i=0; i < numOrders; i++)
+            await utils.createRandomOrder(minDate, maxDate)
+
     } catch (error) {
         console.log(chalk.red('Error when creating orders'))
         console.log(chalk.red('Error: ', error))
