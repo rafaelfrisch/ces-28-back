be72407b62177cd508843a309387231073af777e
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-12-07 18:50:47 -0300

    Adjust Order creation Params

--- src/utils/order/getRandomNumberOfProductsIds.js
+++ src/utils/order/getRandomNumberOfProductsIds.js
@@ -1,12 +1,19 @@
 import * as models from '../../models';
 
+const maxProductsPerOrder = 5
+
+
 const getRandomNumberOfProductsIds = async () => {
     const products = await models.Product.find()
 
     const productsIdsArray = products.map((product) => product._id.toString())
     const shuffledProductsIdsArray = productsIdsArray.sort(() => 0.5 - Math.random())
+    let maxProducts = maxProductsPerOrder
 
-    const randomNumberOfItemsSelected = Math.ceil(Math.random()*shuffledProductsIdsArray.length)
+    if(shuffledProductsIdsArray.length < maxProductsPerOrder){
+        maxProducts = shuffledProductsIdsArray.length
+    }
+    const randomNumberOfItemsSelected = Math.ceil(Math.random()*maxProducts)
     const randomProductsIds = shuffledProductsIdsArray.slice(0, randomNumberOfItemsSelected)
 
     return randomProductsIds
