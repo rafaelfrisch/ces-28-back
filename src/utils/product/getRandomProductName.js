3192a978733691e9f7fa225df7107c6853228faa
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-12-07 22:41:59 -0300

    Adjust imports

--- src/utils/product/getRandomProductName.js
+++ src/utils/product/getRandomProductName.js
@@ -1,6 +1,7 @@
-const productsData = require('./products.json')
+import * as products from './products.json'
 
 const getRandomProductName = () => {
+    const productsData = products.default
     const randomIndex = Math.floor(Math.random()*productsData.length)
     return productsData[randomIndex].name
 }
